40fa50c5f9f04f6382244c28f7f449b8ae27f157
adamohern <adam@mechanicalcolor.com>
2017-07-20 10:24:16 -0400

    fixed noodles loopslice menu

--- Scripts/noodles_loopSliceAuto.py
+++
@@ -1,9 +0,0 @@
-# python
-
-loops = lx.args()[0]
-
-lx.eval('tool.set poly.loopSlice on')
-lx.eval('tool.flag poly.loopSlice auto false')
-lx.eval('tool.attr poly.loopSlice count %s' % loops)
-lx.eval('tool.doApply')
-lx.eval('tool.set poly.loopSlice off')
--- lxserv/noodles_loopSlice.py
+++ lxserv/noodles_loopSlice.py
@@ -17,9 +17,22 @@ class CommandClass(mc_noodles.CommanderClass):
             }
         ]
 
-    def commander_execute(self, msg, flags):
-        loops = not self.commander_arg_value(0)
+    def cmd_Flags(self):
+        # This is the key to this command.
+        # It send the commands as if not called from an external command.
+        # And thus the tool is not refired as a new instance.
+        return lx.symbol.fCMD_QUIET
 
-        lx.eval('@noodles_loopSliceAuto.py %s' % loops)
+    def commander_execute(self, msg, flags):
+        loops = self.commander_arg_value(0)
+
+        if lx.eval1('tool.set poly.loopSlice ?') == 'on':
+            lx.eval('tool.set poly.loopSlice off')
+
+        lx.eval('tool.set poly.loopSlice on')
+        lx.eval('tool.flag poly.loopSlice auto false')
+        lx.eval('tool.attr poly.loopSlice count %s' % loops)
+        lx.eval('tool.doApply')
+        lx.eval('tool.set poly.loopSlice off')
 
 lx.bless(CommandClass, 'noodles.loopSlice')
